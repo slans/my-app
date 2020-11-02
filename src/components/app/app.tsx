1c789fe564d677fff4054e6d8e5ff7617f63bceb
Slava Antonenko <slans2@gmail.com>
2020-11-02 08:11:40 +0200

    Update 1

---
+++ src/components/app/app.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+export class App extends Component {
+	render() {
+		return (
+			<>
+				<div className='header'></div>
+				<div className='sidebar'></div>
+				<div className='main'></div>
+				<div className='footer'></div>
+			</>
+		);
+	}
+}
